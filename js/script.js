4fe6ed1612d0587d164fad4368c1a414b9d65c83
CarlosERM <carlos.eduardo@estudante.ifgoiano.edu.br>
2021-12-03 17:16:58 -0300

    i dont know how to fix the mobile menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log("oi");
+const burgerButton = document.querySelector(".navigation__hamburguer");
+const menu = document.querySelector(".top__mobile-menu");
+const events = ["ontouch", "click"];
+function handleClick(event) {
+  event.preventDefault();
+  menu.classList.toggle("active");
+  //   outsideClick(menu, () => {
+  //     menu.classList.remove("active");
+  //   });
+}
+
+// function outsideClick(element1, callback) {
+//   const html = document.documentElement;
+//   html.addEventListener("click", handleOutsideClick);
+
+//   function handleOutsideClick(event) {
+//     console.log(event.target);
+//   }
+// }
+
+events.forEach((event) => {
+  burgerButton.addEventListener(event, handleClick);
+});
