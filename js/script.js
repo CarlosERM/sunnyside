9063906afa2e2ec46438904381e568be329ee1a4
CarlosERM <carlos.eduardo@estudante.ifgoiano.edu.br>
2021-12-05 14:18:30 -0300

    eslint test

--- js/script.js
+++ js/script.js
@@ -1,21 +1,8 @@
 const menu = document.querySelectorAll("[data-dropdown]");
 
-function handleClick(event) {
-  event.preventDefault();
-  this.classList.add("active");
-  outsideClick(this, ["touchstart", "click"], () => {
-    this.classList.remove("active");
-  });
-}
-
 function outsideClick(element, events, callback) {
   const html = document.documentElement;
   const outside = "data-outside";
-  if (!element.hasAttribute(outside)) {
-    html.addEventListener("click", handleOutsideClick);
-    element.setAttribute(outside, "");
-  }
-
   function handleOutsideClick(event) {
     if (!element.contains(event.target)) {
       element.removeAttribute(outside);
@@ -25,6 +12,19 @@ function outsideClick(element, events, callback) {
       callback();
     }
   }
+
+  if (!element.hasAttribute(outside)) {
+    html.addEventListener("click", handleOutsideClick);
+    element.setAttribute(outside, "");
+  }
+}
+
+function handleClick(event) {
+  event.preventDefault();
+  this.classList.add("active");
+  outsideClick(this, ["touchstart", "click"], () => {
+    this.classList.remove("active");
+  });
 }
 
 menu.forEach((m) => {
